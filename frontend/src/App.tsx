9b3d545685c76a98db26c3ca8e63dd35d5f926d9
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-11-07 17:17:26 +0200

    starting with the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
     <div className="flex min-h-screen flex-col bg-[#e1e1e1]">
       <Navbar />
+      <Home />
     </div>
     </>
   )
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="bg-[#e1e1e1] pb-16 pt-8 px">
+        <div className="mx-auto flex max-w-6xl flex-col gap-8 px-6 lg:flex-row">
+            <aside className="w-full shrink-0 lg:w-72">
+                <Filters />
+            </aside>
+
+            
+        </div>
+    </div>
+  )
+}
