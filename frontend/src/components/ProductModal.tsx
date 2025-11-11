dee45a59ce0ca9ced6fe451bfdea7c2cd5eabb9b
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-11-11 18:36:58 +0200

    set the default quantity into 1

--- frontend/src/components/ProductModal.tsx
+++ frontend/src/components/ProductModal.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useStore } from "../store/useStore";
 
 const currency = new Intl.NumberFormat("en-US", {
     style:"currency",
     currency:"USD",
     maximumFractionDigits: 2,
 });
+
 export default function ProductModal() {
+    const { products, selectedProductId, closeProductModal, addToCart } = useStore();
+    const [quantity, setQuantity] = useState(1);
   return (
-    <div>ProductModal</div>
+    <div>Product</div>
   )
 }
