5187298503c2a534f36899e63bda7cde8e1bbb0b
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-11-12 15:23:11 +0200

    adding routes to navigate between pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import ProductModal from "./components/ProductModal";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col bg-[#e1e1e1]">
         <Navbar />
-        <Home />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
       <ProductModal />
