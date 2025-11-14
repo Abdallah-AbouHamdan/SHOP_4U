db2da8b7a3a0876bbc176a71e60bbac70f3cc878
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-11-14 15:07:12 +0200

    adding the page introduction  in the section

--- frontend/src/pages/Favorites.tsx
+++ frontend/src/pages/Favorites.tsx
@@ -8,6 +8,21 @@ export default function Favorites() {
         [favorites, products]
     )
   return (
-    <div>Favorites</div>
+    <div className='bg-[#ffffff] pb-16 pt-8 min-h-screen'>
+        <section className='mx-auto mb-10 w-11/12 max-w-6xl rounded-3xl border border-slate-200/60 md:p-10'>
+            <div className="space-y-3">
+                <p className='text-[0.6rem] font-semibold uppercase tracking-[0.3em] text-slate-500 sm:text-xs'>
+                    Wishlist
+                </p>
+                <h1 className="text-2xl font-semibold leading-tight text-slate-900 sm:text-3xl md:text-4xl">
+                    Your favorites
+                </h1>
+                <p className="text-sm text-slate-500 sm:text-base">
+                    Save items you love fore quick access later.
+                </p>
+            </div>
+        </section>
+        
+    </div>
   )
 }
