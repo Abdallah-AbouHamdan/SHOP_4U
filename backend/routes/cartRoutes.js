4b65a2d4925154abae3d1e1f972aeb97e7601dac
Abdallah Abou Hamdan <abdallahabouhamdan@Abdallahs-MacBook-Air.local>
2025-12-12 12:24:46 +0200

    cart routes

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  getCart,
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+  clearCart,
+} from "../controllers/cartController.js";
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+
+router.get("/", auth, getCart);
+
+
+router.post("/", auth, addToCart);
+
+
+router.put("/", auth, updateCartItem);
+
+
+router.delete("/:productId", auth, removeFromCart);
+
+router.delete("/", auth, clearCart);
+
+export default router;
